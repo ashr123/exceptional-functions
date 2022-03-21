207fe8700be5e953b5773f7f3742c88cf5d6e6d7
Roy Ash <roy.ash456@gmail.com>
2022-03-22 00:52:44 +0200

    Fixed small bug
    Signed-off-by: Roy Ash <roy.ash456@gmail.com>

--- src/main/java/io/github/ashr123/exceptional/functions/ThrowingUnaryOperator.java
+++ src/main/java/io/github/ashr123/exceptional/functions/ThrowingUnaryOperator.java
@@ -3,24 +3,10 @@ package io.github.ashr123.exceptional.functions;
 import java.util.function.UnaryOperator;
 
 @FunctionalInterface
-public interface ThrowingUnaryOperator<T> extends UnaryOperator<T>
+public interface ThrowingUnaryOperator<T> extends UnaryOperator<T>, ThrowingFunction<T, T>
 {
 	static <T> UnaryOperator<T> unchecked(ThrowingUnaryOperator<T> throwingUnaryOperator)
 	{
 		return throwingUnaryOperator;
 	}
-
-	@Override
-	default T apply(T t)
-	{
-		try
-		{
-			return applyThrows(t);
-		} catch (Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
-
-	T applyThrows(T t) throws Exception;
 }
